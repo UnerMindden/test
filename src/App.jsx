364abe3b52fbce57ad48f7a90562d34154ad26b6
UnerMindden <63872704+UnerMindden@users.noreply.github.com>
2023-04-12 20:14:05 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import { Button, Form, InputGroup, Modal } from "react-bootstrap";
+import { Badge, Button, Col, Container, Form, InputGroup, Modal, Nav, Navbar, Row, Stack } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css"
 import Popup from "./components/Popup";
+import StarRating from "./components/StarRating";
 
 function App() {
 
@@ -19,10 +20,12 @@ function App() {
   const [show, setShow] = useState(false);
   const [accept, setAccept] = useState("")
   const [decline, setDecline] = useState("")
+  const [collegue, setCollegue] = useState(false)
 
   const handleClose = () => {
     setAccept("")
     setDecline("")
+    setCollegue(false)
     setShow(false)
   };
 
@@ -36,72 +39,136 @@ function App() {
     setDecline("К сожалению, мы не можем вас пригласить")
   }
 
+  const handleShowCollegue = () => {
+    setShow(true)
+    setCollegue(true)
+  }
+
 
 
   return (
     <>
       <header>
         <link rel='stylesheet' href='//www.cameratag.com/v15/css/cameratag.css' />
-        <nav>
-          <ul>
-            <li>МОИ ВАКАНСИИ</li>
-            <li>ОТКЛИКИ</li>
-            <li>МОИ ВИДЕОИНТЕРВЬЮ</li>
-            <li>АККАУНТ</li>
-          </ul>
+        <nav style={{ "marginBottom": "8vh" }}>
+          <Navbar expand="sm" bg="dark" variant="dark">
+            <Container>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="#features">
+                    МОИ ВАКАНСИИ <Badge bg="success">1</Badge>
+                  </Nav.Link>
+                  <Nav.Link>
+                    ОТКЛИКИ <Badge bg="success">10</Badge>
+                  </Nav.Link>
+                  <Nav.Link>
+                    МОИ ВИДЕОИНТЕРВЬЮ <Badge bg="success">2</Badge>
+                  </Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link>АККАУНТ</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
         </nav>
       </header>
-      <div className="container">
-        <div className="main-content">
-          <div className="button-list">
-            <ul>
-              <li className="contact-button"><Button variant="success" onClick={handleShowAccept}>Связаться</Button></li>
-              <li className="forward-button" >Отправить</li>
-              <li className="deny-button" onClick={handleShowDismiss}>Отказать</li>
-              <li className="list-accept-or-not">
-                <Form.Select size="sm">
-                  <option>В ожиданий</option>
-                  <option>Отказ</option>
-                  <option>Прошел</option>
-                  <option>Принят на работу</option>
-                </Form.Select>
-              </li>
-            </ul>
+      <Container id="container">
+        <Row>
+          <Col sm={7} style={{ "margin": "0 2vh" }}>
+            <Row className="main-content">
+              <Row className="main-buttons">
+                <Col sm={3}>
+                  <p className="contact-button"><Button variant="success" onClick={handleShowAccept}>Связаться</Button></p>
+                </Col>
+                <Col sm={3}>
+                  <p className="forward-button" onClick={handleShowCollegue}>Отправить</p>
+                </Col>
+                <Col sm={2}>
+                  <p className="deny-button" onClick={handleShowDismiss}>Отказать</p>
+                </Col>
+                <Col xs={4}>
+                  <p className="list-accept-or-not">
+                    <Form.Select size="sm">
+                      <option>В ожиданий</option>
+                      <option>Отказ</option>
+                      <option>Прошел</option>
+                      <option>Принят на работу</option>
+                    </Form.Select>
+                  </p>
+                </Col>
+                
+                
+                <Popup accept={accept} decline={decline} show={show} handleClose={handleClose} collegue={collegue} />
+                
+              </Row>
+              <Row>
+                <Col style={{ "padding": "0 20vh" }} className="player">
+                  <player id='myFirstPlayer' data-download='true' data-assets='["v-deb3c9e6-c92e-4673-91f7-88baa7793040"]'
+                    data-aspectratio='automatic' data-app-id='a-a2cfdae0-ba54-013b-3e40-02c7c2460d41'></player>
+                </Col>
+              </Row>
+              <Row className="rates-candidate">
+                <Col>
+                  <p>Оценка кандидата</p>
+                </Col>
+              </Row>
+              <Row style={{ "padding": "0 40vh" }}>
+                <Col>
+                  <StarRating />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div>
 
-            <Popup accept={accept} decline={decline} show={show} handleClose={handleClose} />
-          </div>
-          <div>
-            <player id='myFirstPlayer' data-download='true' data-assets='["v-deb3c9e6-c92e-4673-91f7-88baa7793040"]'
-              data-aspectratio='automatic' data-app-id='a-a2cfdae0-ba54-013b-3e40-02c7c2460d41'></player>
-          </div>
-          <div className="rate">
-            <div>
-              <p>Оценка кандидата</p>
-            </div>
-            <div>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <InputGroup>
+                    <Form.Control as="textarea" defaultValue="Первый коммент" placeHolder="Комментарий">
+                    </Form.Control>
+                  </InputGroup>
+                </Col>
+              </Row>
+            </Row>
+          </Col>
 
-            </div>
-            <div className="comments">
-              <textarea>
-              </textarea>
-            </div>
-          </div>
-        </div>
-        <div className="candidate-list">
-          <div>
-            <p>Кандидаты</p>
-          </div>
-        </div>
-        <div className="rates-chat-contact">
-          <div>
-            <ul>
-              <li>Оценки</li>
-              <li>Чат</li>
-              <li>Контакты</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+          <Col style={{ "backgroundColor": "white", "borderRadius": "20px", "padding": "5vh" }} sm={2} xs={{ order: "first" }}>
+            <Row>
+              <Col>
+                <p>Кандидаты</p>
+              </Col>
+            </Row>
+          </Col>
+
+
+          <Col style={{ "backgroundColor": "white", "borderRadius": "20px", "padding": "5vh" }}>
+            <Row>
+              <Col sm={4}>
+                <p>Оценки</p>
+                <Row style={{ "width": "30vh" }} >
+                  <Col>Общий уровень</Col>
+                  <Row><StarRating /></Row>
+                </Row>
+                <Row style={{ "width": "30vh" }} >
+                  <Col>Оценка коллег</Col>
+                  <Row><StarRating /></Row>
+                </Row>
+              </Col>
+              <Col>
+                <p>Чат</p>
+              </Col>
+              <Col>
+                <p>Контакты</p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,49 +1,73 @@
 import { Button, Form, InputGroup, Modal } from "react-bootstrap";
 import "../App.css"
 
-function Popup({ show, handleClose, accept, decline }) {
+function Popup({ show, handleClose, accept, decline, collegue }) {
 
     return (
         <div>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Отправить письмо</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div className="modal-body">
-                        <div className="modal-select">
-                            <Form.Select>
-                                <option>mail</option>
-                                <option>mail</option>
-                                <option>mail</option>
-                            </Form.Select>
-                        </div>
+            {collegue
+                ?
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Отправить коллеге</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
                         <div className="modal-input">
                             <InputGroup className="mb-3">
                                 <Form.Control
                                     aria-label="Default"
                                     aria-describedby="inputGroup-sizing-default"
-                                    defaultValue={decline ? decline : accept}
+                                    placeholder="Email"
                                 />
                             </InputGroup>
                         </div>
-                        <div className="modal-textarea">
-                            <InputGroup>
-                                <Form.Control defaultValue="Здравствуйте!" as="textarea" />
-                            </InputGroup>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="success" onClick={handleClose}>
+                            ОТПРАВИТЬ
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                :
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Отправить письмо</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="modal-body">
+                            <div className="modal-select">
+                                <Form.Select>
+                                    <option>mail</option>
+                                    <option>mail</option>
+                                    <option>mail</option>
+                                </Form.Select>
+                            </div>
+                            <div className="modal-input">
+                                <InputGroup className="mb-3">
+                                    <Form.Control
+                                        aria-label="Default"
+                                        aria-describedby="inputGroup-sizing-default"
+                                        defaultValue={decline ? decline : accept}
+                                    />
+                                </InputGroup>
+                            </div>
+                            <div className="modal-textarea">
+                                <InputGroup>
+                                    <Form.Control defaultValue={decline ? "Вы не приняты!": "Вы приняты"} as="textarea" />
+                                </InputGroup>
+                            </div>
                         </div>
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="success" onClick={handleClose}>
-                        ОТПРАВИТЬ
-                    </Button>
-                    <Button variant="danger" onClick={handleClose}>
-                        ОТМЕНА
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="success" onClick={handleClose}>
+                            ОТПРАВИТЬ
+                        </Button>
+                        <Button variant="danger" onClick={handleClose}>
+                            ОТМЕНА
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            }
         </div>
     );
 }
